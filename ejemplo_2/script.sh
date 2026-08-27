ffad3d22cf21cf21ddcce9eb5d93a96361d7958d
MgtDevNet <mgutierrezt98@gmail.com>
2026-08-26 20:48:34 -0500

    seguimiento scripting

--- ejemplo_2/script.sh
+++ ejemplo_2/script.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # MI primer script
 echo "Hola, este es mi primer script"
+echo "Tu directorio acutal es: $(pwd)" #para concatenas comandos con texto
+echo "Fecha de hoy  $(ncal -b)" 
+name="manu"
+echo "hola $name"
+
