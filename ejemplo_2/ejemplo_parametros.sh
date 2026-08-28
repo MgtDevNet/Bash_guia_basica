debb4713b6adf7ac8b90110ecd636497e3d2b1a0
MgtDevNet <mgutierrezt98@gmail.com>
2026-08-27 23:47:29 -0500

    argumentos y parámetros

---
+++ ejemplo_2/ejemplo_parametros.sh
@@ -0,0 +1,5 @@
+echo "El nombre del script es: $0"
+echo "El primero parámetro es : $1"
+echo "El segundo parámetro es : $2"
+echo "Número de parámetros: $#"
+echo "Listado de todos los parámetros:" $*
---
+++ ejemplo_2/entrada_datos.sh
@@ -0,0 +1,8 @@
+echo "¿Cuál es tu nombre?"
+read name # acá decimos que es una entrada de datos
+#y se guardará en al variable name
+echo "En hora buena, te envio un fraterno saludo $name"
+read -p "¿Cuál es tu edad?" age
+echo Tu edad es $age
+read -s password
+echo !Contraseña guardada exitosamente¡
